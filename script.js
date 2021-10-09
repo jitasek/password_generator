f2e12c970fcfe65873d72ff49171cc3278e64242
jitasek <jitka.moreira@gmail.com>
2021-10-10 00:30:25 +0200

    final version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// 4 Functions to generate the desired types of characters for the password based on user inputs
+// Initiations
 var nums = "0123456789";
 var lowLetters = "abcdefghijklmnopqrstuvwxyz";
 var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -11,150 +11,110 @@ var InputNumber;
 var InputLowLetter;
 var InputUpLetter;
 var InputSpecial;
+var userChoice = "";
+var guaranteedChoices = [];
+var resultArray = [];
 
-function userInputNumbers () {
-  //var nums = "0123456789";
-  numLength = nums.length;
-  var userChoiceNumbers = confirm("Do you want to include numbers?");
-  if (userChoiceNumbers === true) {
-    randomNumber = nums.charAt(Math.floor(Math.random() * numLength));
-    return randomNumber;
-  } else {
-    return false;
-  } 
-}
-//console.log(userInputNumbers(1));
+// Generate random characters
 
-function userInputLowLetters () {
-  //var lowLetters = "abcdefghijklmnopqrstuvwxyz";
-  lowLength = lowLetters.length;
-  var userChoiceLowLetters = confirm ("Do you want to include lowercase letters?");
-  if (userChoiceLowLetters === true) {
-    randomLowLetter = lowLetters.charAt(Math.floor(Math.random() * lowLength));
-    return randomLowLetter;
-  } else {
-    return false;
-  }
+function getRandomNumber() {
+  var randomNumber = nums.charAt(Math.floor(Math.random() * nums.length));
+  return randomNumber;
 }
-//console.log(userInputLowLetters(1));
 
-function userInputUpLetters () {
-  //var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  upLength = upLetters.length;
-  var userChoiceUpLetters = confirm("Do you want to include uppercase letters?");
-  if (userChoiceUpLetters === true) {
-    randomUpLetter = upLetters.charAt(Math.floor(Math.random() * upLength));
-    return randomUpLetter;
-  } else {
-    return false;
-  } 
+function getRandomLowLetter() {
+  var randomLowLetter = lowLetters.charAt(
+    Math.floor(Math.random() * lowLetters.length)
+  );
+  return randomLowLetter;
 }
-//console.log(userInputUpLetters(1));
 
-function userInputSpecials () {
-  //var specials = "!@#$%^&*()";
-  specLength = specials.length;
-  var userChoiceSpecials = confirm("Do you want to include special characters?");
-  if (userChoiceSpecials === true) {
-    randomSpecial = specials.charAt(Math.floor(Math.random() * specLength));
-    return randomSpecial;
-  } else {
-    return false;
-  } 
+function getRandomUpLetter() {
+  var randomUpLetter = upLetters.charAt(
+    Math.floor(Math.random() * upLetters.length)
+  );
+  return randomUpLetter;
 }
-//console.log(userInputSpecials(1));
+
+function getRandomSpecial() {
+  var randomSpecial = specials.charAt(
+    Math.floor(Math.random() * specials.length)
+  );
+  return randomSpecial;
+}
 
 // Function to generate password
 
 function generatePassword() {
-  howManyChars = parseInt(prompt("How many characters will your password have? Choose between 8 and 128"));
+  howManyChars = parseInt(
+    prompt(
+      "How many characters will your password have? Choose between 8 and 128"
+    )
+  );
 
-  if(howManyChars === false) {
+  if (howManyChars === false) {
     alert("Number of characters needed.");
-  } else if (howManyChars < 8 || howManyChars > 128) {
-    howManyChars = parseInt(prompt("Please choose between 8 and 128"));
-  } else {
-    InputNumber = userInputNumbers();
-    InputLowLetter = userInputLowLetters();
-    InputUpLetter = userInputUpLetters();
-    InputSpecial = userInputSpecials();
   }
+  if (howManyChars < 8 || howManyChars > 128) {
+    howManyChars = parseInt(prompt("Please choose between 8 and 128"));
+  }
+  //console.log(howManyChars);
+  // User input - variables hold the true/false
+  InputNumber = confirm("Dou you wanna include numbers?");
+  InputLowLetter = confirm("Dou you wanna include lowercase letters?");
+  InputUpLetter = confirm("Dou you wanna include uppercase letters?");
+  InputSpecial = confirm("Dou you wanna include special characters?");
 
   // 4 negative choices
-  if (InputNumber === false && InputLowLetter === false && InputUpLetter === false && InputSpecial ===false) {
-    alert("You must select some criteria.");   // or do I need userChoice = at the beginning? 
-  } 
+  if (!InputNumber && !InputLowLetter && !InputUpLetter && !InputSpecial) {
+    alert("You must select some criteria.");
+  }
   // 4 positive choices
-  else if (InputNumber && InputLowLetter && InputUpLetter && InputSpecial) {
-    userChoice = InputNumber.concat(InputLowLetter, InputUpLetter, InputSpecial);
-  } 
-  // 3 positive choices
-  else if (InputNumber && InputLowLetter && InputUpLetter) {
-    userChoice = InputNumber.concat(InputLowLetter, InputUpLetter);
+  if (InputNumber) {
+    userChoice = userChoice.concat(nums);
+    guaranteedChoices.push(getRandomNumber());
   }
-  else if (InputNumber && InputLowLetter && InputSpecial) {
-    userChoice = InputNumber.concat(InputLowLetter, InputSpecial);
+  if (InputLowLetter) {
+    userChoice = userChoice.concat(lowLetters);
+    guaranteedChoices.push(getRandomLowLetter());
   }
-  else if (InputNumber && InputUpLetter && InputSpecial) {
-    userChoice = InputNumber.concat(InputUpLetter, InputSpecial);
+  if (InputUpLetter) {
+    userChoice = userChoice.concat(upLetters);
+    guaranteedChoices.push(getRandomUpLetter());
   }
-  else if (InputLowLetter && InputUpLetter && InputSpecial) {
-    userChoice = InputLowLetter.concat(InputUpLetter, InputSpecial);
+  if (InputSpecial) {
+    userChoice = userChoice.concat(specials);
+    guaranteedChoices.push(getRandomSpecial());
   }
-  // 2 positive choices
-  else if (InputNumber && InputLowLetter) {
-    userChoice = InputNumber.concat(InputLowLetter);
-  }
-  else if (InputNumber && InputUpLetter) {
-    userChoice = InputNumber.concat(InputUpLetter);
-  }
-  else if (InputNumber && InputSpecial) {
-    userChoice = InputNumber.concat(InputSpecial);
-  }
-  else if (InputLowLetter && InputUpLetter) {
-    userChoice = InputLowLetter.concat(InputUpLetter);
-  }
-  else if (InputLowLetter && InputSpecial) {
-    userChoice = InputLowLetter.concat(InputSpecial);
-  }
-  else if (InputUpLetter && InputSpecial) {
-    userChoice = InputUpLetter.concat(InputSpecial);
-  }
+  //console.log(userChoice);
+  //console.log(guaranteedChoices);
 
-  // 1 positive choice
-  else if (userInputNumbers) {
-    userChoice = InputNumber;
-  }
-  else if (userInputLowLetters) {
-    userChoice = InputLowLetter;
-  }
-  else if (userInputUpLetters) {
-    userChoice = InputUpLetter;
-  }
-  else if (userInputSpecials) {
-    userChoice = InputSpecial;
-  }
-
-  // I need to let the functions run "howManyChars" times   ??
-  var pickedChoices = ""; 
   for (var i = 0; i < howManyChars; i++) {
-    pickedChoices = userChoic
-  } 
+    var index = Math.floor(Math.random() * userChoice.length);
+    resultArray.push(userChoice[index]);
+    //console.log(index);
+    //console.log(userChoice[index]);
+  }
+  //console.log(resultArray);
+  for (var j = 0; j < guaranteedChoices.length; j++) {
+    resultArray[j] = guaranteedChoices[j];
+  }
+  console.log(resultArray);
+  return resultArray.join("");
 }
 
 // Write password to the #password input
 
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // // Assignment Code - OLD CODE, PLEASE IGNORE:
